56f186795e20b014b3f782814eee1a7b972257e8
Tsue_Hic22009 <hic22009@byui.edu>
2024-01-25 09:17:33 -0700

    created DOM

---
+++ week2/scripts/bom.js
@@ -0,0 +1,26 @@
+const input= document.querySelector('#favchap');
+const button= document.querySelector('#button');
+const list= document.querySelector('#list');
+
+button.addEventListener('click', function()  {
+    const value = input.value;
+    if (input.value){
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+
+        li.textContent = input.value;
+
+        deleteButton.textContent = '❌';
+
+        li.appendChild(deleteButton);
+
+        list.appendChild(li);
+        
+    } else {
+        input.focus();
+    }
+
+});
+// button.addEventlistener('click', () => {
+
+// });
