4e7e9f86a92e18d7a83b922882e50684f6f0ab1f
Tsue_Hic22009 <hic22009@byui.edu>
2024-03-09 10:31:46 -0700

    prophets assignment

---
+++ week05/scripts/prophet.js
@@ -0,0 +1,31 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+
+async function getProphetData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  // console.table(data.prophets); // temporary testing of data retrieval
+  displayProphets(data.prophets);
+}
+
+getProphetData();
+
+const displayProphets = (prophets) => {
+  prophets.forEach((prophet) => {
+    let card = document.createElement('section');
+    let fullName = document.createElement('h2');
+    let portrait = document.createElement('img');
+
+    fullName.textContent = `${prophet.firstName} ${prophet.lastName}`;
+    portrait.setAttribute('src', prophet.imageurl);
+    portrait.setAttribute('alt', `Portrait of ${prophet.firstName} ${prophet.lastName}`);
+    portrait.setAttribute('loading', 'lazy');
+    portrait.setAttribute('width', '340');
+    portrait.setAttribute('height', '440');
+
+    card.appendChild(fullName);
+    card.appendChild(portrait);
+
+    cards.appendChild(card);
+  });
+}
